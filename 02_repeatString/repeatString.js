c864df319d40474af1accce77ceb3c3a398787b2
hitesh <hiteshtripathi91@gmail.com>
2024-08-06 10:29:07 +0530

    solved repeatstring sum

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(a,b) {
+    let st=''
+    for(let i=0;i<b;i++){
+        st=st+a
+    }
+    if(b<0){
+        return 'ERROR'
+    }
+    else{
+        return st
+    }
 };
 
 // Do not edit below this line
